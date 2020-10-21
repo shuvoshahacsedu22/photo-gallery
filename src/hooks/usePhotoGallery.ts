44e2548e637fef28ccad59c1f7cabd9bf54e90c2
shuvoshaha7 <shuvo.shaha@tigerit.com>
2020-10-21 21:21:28 +0600

    Saved picture in device storage and retrive on loading the pwa

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -9,39 +9,59 @@ export interface Photo {
     filepath: string;
     webviewPath?: string;
   }
-
+const PHOTO_STORAGE = "photos";
 export function usePhotoGallery() {
+    const { get, set } = useStorage();
     const [photos, setPhotos] = useState<Photo[]>([]);
     const { getPhoto } = useCamera();
     const { deleteFile, getUri, readFile, writeFile } = useFilesystem();
+   
+    useEffect(() => {
+      const loadSaved = async () => {
+        const photosString = await get(PHOTO_STORAGE);
+        const photos = (photosString ? JSON.parse(photosString) : []) as Photo[];
+        for (let photo of photos) {
+          const file = await readFile({
+            path: photo.filepath,
+            directory: FilesystemDirectory.Data
+          });
+          photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+        }
+        setPhotos(photos);
+      };
+      loadSaved();
+    }, [get, readFile]);
+
+
     const takePhoto = async () => {
-        const cameraPhoto = await getPhoto({
-          resultType: CameraResultType.Uri,
-          source: CameraSource.Camera,
-          quality: 100
-        });
-           
-        const fileName = new Date().getTime() + '.jpeg';
-        const savedFileImage = await savePicture(cameraPhoto, fileName);
-        const newPhotos = [savedFileImage, ...photos];
-        setPhotos(newPhotos);
+      const cameraPhoto = await getPhoto({
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Camera,
+        quality: 100
+      });
+    
+      const fileName = new Date().getTime() + '.jpeg';
+      const savedFileImage = await savePicture(cameraPhoto, fileName);
+      const newPhotos = [savedFileImage, ...photos];
+      setPhotos(newPhotos);
+      set(PHOTO_STORAGE, JSON.stringify(newPhotos));
     };
     
     const savePicture = async (photo: CameraPhoto, fileName: string): Promise<Photo> => {
-        const base64Data = await base64FromPath(photo.webPath!);
-        const savedFile = await writeFile({
-          path: fileName,
-          data: base64Data,
-          directory: FilesystemDirectory.Data
-        });
-      
-        // Use webPath to display the new image instead of base64 since it's
-        // already loaded into memory
-        return {
-          filepath: fileName,
-          webviewPath: photo.webPath
-        };
+      const base64Data = await base64FromPath(photo.webPath!);
+      const savedFile = await writeFile({
+        path: fileName,
+        data: base64Data,
+        directory: FilesystemDirectory.Data
+      });
+    
+      // Use webPath to display the new image instead of base64 since it's
+      // already loaded into memory
+      return {
+        filepath: fileName,
+        webviewPath: photo.webPath
       };
+    };
     return {
       photos,
       takePhoto
